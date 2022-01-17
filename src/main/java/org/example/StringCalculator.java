f381f7a982fe7b1ded5c5f7882d8e1830f85dddb
Andra <andra@wdc-creative.com>
2022-01-17 15:27:01 +0000

    steps 1 to 6

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,14 +3,37 @@ package org.example;
 
 public class StringCalculator
 {
-    public static int add(String string )
-    {
+    public static int add(String string ) throws Exception {
         int result = 0;
+
+        if (string.contains("-")) {
+            String[] numbersInString = string.split(",");
+            String negativeNumbersString = "";
+            for (int i = 0; i < numbersInString.length; i++) {
+                int parsedInt = Integer.parseInt(numbersInString[i]);
+                if (parsedInt < 0) {
+                    if (i > 0) {
+                        negativeNumbersString += ",";
+                    }
+                    negativeNumbersString += numbersInString[i];
+                }
+            }
+            throw new Exception("negatives not allowed: " + negativeNumbersString);
+        }
+
+        if (string.contains("//")) {
+            char separator = string.charAt(2);
+          string = string.substring(4);
+          string = string.replace(separator, ',');
+        }
+
         string = string.replace("\n", ",");
         if (string.contains(",")) {
             String[] numbersInString = string.split(",");
             for (String number : numbersInString) {
-                result += Integer.parseInt(number);
+                if (Integer.parseInt(number) <= 1000) {
+                    result += Integer.parseInt(number);
+                }
             }
         } else if (string.length() > 0){
             result = Integer.parseInt(string);
--- src/test/java/org/example/StringCalculatorShould.java
+++ src/test/java/org/example/StringCalculatorShould.java
@@ -10,26 +10,70 @@ public class StringCalculatorShould
 {
 
     @Test
-    public void should_return_single_value()
-    {
+    public void should_return_single_value() throws Exception {
         assertEquals(0, add(""));
         assertEquals(4, add("4"));
         assertEquals(7, add("7"));
     }
 
     @Test
-    public void should_return_sum_of_multiple_values_divided_by_commas(){
+    public void should_return_sum_of_multiple_values_divided_by_commas() throws Exception {
         assertEquals(3, add("1,2"));
         assertEquals(4, add("2,2"));
         assertEquals(45, add("1,2,3,4,5,6,7,8,9"));
     }
 
     @Test
-    public void should_return_sum_when_new_line_separator_is_used(){
+    public void should_return_sum_when_new_line_separator_is_used() throws Exception {
         assertEquals(6, add("1\n2,3"));
         assertEquals(8, add("1\n2,3\n2"));
 
     }
 
+    @Test
+    public void should_return_sum_when_using_custom_seperator() throws Exception {
+        assertEquals(3, add("//;\n1;2"));
+        assertEquals(4, add("//X\n2X2"));
+    }
 
+    @Test
+    public void should_return_sum_while_ignoring_numbers_greater_than_1000() throws Exception {
+        assertEquals(2, add("1001,2"));
+        assertEquals(5, add("2055,5"));
+    }
+
+
+    @Test
+    public void should_throw_error_on_negative_input() throws Exception {
+        try {
+            add("-1");
+        }
+        catch (Exception e) {
+            final String expected = "negatives not allowed: -1";
+            assertEquals( expected, e.getMessage());
+        }
+
+        try {
+            add("-2");
+        }
+        catch (Exception e) {
+            final String expected = "negatives not allowed: -2";
+            assertEquals( expected, e.getMessage());
+        }
+
+        try {
+            add("-1,-2");
+        }
+        catch (Exception e) {
+            final String expected = "negatives not allowed: -1,-2";
+            assertEquals( expected, e.getMessage());
+        }
+        try {
+            add("-1,-2,-5,10");
+        }
+        catch (Exception e) {
+            final String expected = "negatives not allowed: -1,-2,-5";
+            assertEquals( expected, e.getMessage());
+        }
+    }
 }
